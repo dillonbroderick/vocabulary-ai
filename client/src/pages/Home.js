04c6ee10c7323e4a2e24ad01592cc9a044a009de
Dillon Broderick <dillon@Dillons-MacBook-Air.local>
2024-04-02 23:02:43 -0700

    CSS makes me want to die

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,35 @@
 import './Home.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function Home() {
+    const [wordOfDay, setWordOfDay] = useState(null);
+    const [animationFinished, setAnimationFinished] = useState(false);
+
+    useEffect(() => {
+        axios.defaults.baseURL = 'http://localhost:4000';
+        axios
+            .get('/api/words/get-word-of-day')
+            .then(res => {
+                setWordOfDay(res.data);
+            })
+            .catch(res => {
+                console.log("error");
+            })
+    }, []);
+
+    const handleAnimationEnd = () => {
+        setAnimationFinished(true);
+    };
+
     return (
         <>
-            <div class="container">
-                <div class="typed-out">today's word is:</div>
-            </div>
+            <div class="typed-out-title" onAnimationEnd={handleAnimationEnd}>today's word is:</div>
+            { animationFinished && (
+                <div class="typed-out-word">
+                    { wordOfDay ? wordOfDay.word : "NO WORD OF DAY TODAY" }
+                </div>
+            )}
         </>
     );
 }
--- server/index.js
+++ server/index.js
@@ -1,19 +1,21 @@
 require('dotenv').config()
 
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const wordRouter = require("./routers/WordRouter");
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 
 app.use((req, res, next) => {
     console.log(req.path, req.method);
     next();
 })
 
-app.use('/api/words', wordRouter)
+app.use('/api/words', wordRouter);
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
